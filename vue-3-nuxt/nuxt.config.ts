4ba34269427d7445e156d974abc73842309bfa47
Eduard Krivanek <krivanek1234@gmail.com>
2023-05-08 17:35:34 +0300

    chore(nuxt): added devtools

--- vue-3-nuxt/nuxt.config.ts
+++ vue-3-nuxt/nuxt.config.ts
@@ -1,14 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['~/assets/scss/styles.scss'],
+  ssr: true,
+  components: true,
+  devtools: {
+    enabled: true
+  },
+  // typescript: {
+  //   strict: true,
+  //   typeCheck: true
+  // },
   modules: [
     'nuxt-icon',
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt',
-    ['@pinia/nuxt',
+    '@nuxt/devtools',
+    [
+      '@pinia/nuxt',
       {
         autoImports: ['defineStore', 'acceptHMRUpdate']
       }
     ]
   ]
-})
+});
